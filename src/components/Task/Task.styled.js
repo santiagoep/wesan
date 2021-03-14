63f6db29db1c22ae0b35b42435e1c17d0ae6b10f
Wendy <wendy.calderon@esinergia.co>
2021-03-14 11:12:21 -0500

    styles were added for tags and priority field

--- src/components/Task/Task.styled.js
+++ src/components/Task/Task.styled.js
@@ -10,8 +10,17 @@ export const StyledDescription = styled.p`
 
 export const StyledPriority = styled.span`
     color: ${({theme}) => theme.colors.white};
+    background-color:${({theme}) => theme.colors.info};
+    border-radius: 3px;
+    line-height: 32px;
+    padding: 0 12px;
+    width: auto;
 `;
 
 export const StyledTags = styled.span`
     color: ${({theme}) => theme.colors.white};
+    height: 16px;
+    line-height: 16px;
+    padding: 0 8px;
+    max-width: 198px;
 `;
